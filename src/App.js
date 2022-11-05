ad03e3b9033052c1bc9c15394b08ba07f45755aa
Samueal Louis Dsouza <dsouzasamueal07@gmail.com>
2022-11-05 21:53:07 +0530

    adding Binge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Section from "./Components/Section";
+import Subscription from "./Components/Subscription";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
+      <Section/>
+      <Subscription/>
+      <Cards/>
+      <Footer/>
     </div>
   );
 }
